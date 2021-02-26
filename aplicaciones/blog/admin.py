cac30c5e056a13fc0a201875d889bb09afd4635b
FernandoNoguera <fnoguerav25@gmail.com>
2021-02-26 11:06:59 -0300

    se agrega el import export al admin

--- aplicaciones/blog/admin.py
+++ aplicaciones/blog/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
+
+class CategoriaResource(resources.ModelResource):
+    class Meta:
+        models = Categoria
+
+
+class CategoriaAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    search_fields = ['nombre'] # para activar la barra de busqueda dentro del adminstrador
+    list_display = ('id','nombre','estado', 'fecha_creacion') # para visualizar los diferentes campos en el encabezado de la tabla del admin
+    resources_class = CategoriaResource
+
+
+class AutorResource(resources.ModelResource):
+    class Meta:
+        models = Autor
+
+class AutorAdmin(ImportExportModelAdmin,admin.ModelAdmin):
+    search_fields = ['nombres','apellidos']
+    list_display = ('id','nombres','apellidos','correo','estado', 'fecha_creacion')
+    resources_class = AutorResource
+
+
+
+admin.site.register(Categoria,CategoriaAdmin)
+admin.site.register(Autor,AutorAdmin)
--- aplicaciones/blog/models.py
+++ aplicaciones/blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField('Nombre de Categoría', max_length =50)
+    estado = models.BooleanField('Categoría Activada/Desactivada', default=True)
+    fecha_creacion = models.DateTimeField('Fecha de creación', auto_now_add=True)
+    fecha_edicion = models.DateTimeField('Fecha de edición', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Categoría'
+        verbose_name_plural = 'Categorías'
+
+    def __str__(self):
+        return self.nombre
+
+
+class Autor(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombres = models.CharField('Nombres de autor', max_length=200)
+    apellidos = models.CharField('Apellidos de autor', max_length=200)
+    facebook = models.URLField('Facebook', null=True, blank=True)
+    twitter = models.URLField('Twitter', null=True, blank=True)
+    instagram = models.URLField('Instagram', null=True, blank=True)
+    web = models.URLField('Web', null=True, blank=True)
+    correo = models.EmailField('Correo Eléctronico')
+    estado = models.BooleanField('Autor Activada/Desactivada', default=True)
+    fecha_creacion = models.DateTimeField('Fecha de creación', auto_now_add=True)
+    fecha_edicion = models.DateTimeField('Fecha de edición', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
+
+    def __str__(self):
+        return '{0},{1}'.format(self.apellidos,self.nombres)
+
